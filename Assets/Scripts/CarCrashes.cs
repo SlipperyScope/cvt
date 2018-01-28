5ca75a75679df5f07fc73eb9447deeb84ef5190a
ADKauth <adamkauth@gmail.com>
2018-01-27 23:34:07 -0800

    Moved basic track hit testing to Racetrack scene

---
+++ Assets/Scripts/CarCrashes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCrashes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("track"))
+        {
+            Car_Controls controls = GetComponent<Car_Controls>();
+            controls.health -= 100;
+        } 
+    }
+}
